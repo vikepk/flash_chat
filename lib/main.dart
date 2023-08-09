3143ea406bc27a30d088244bc01d659812339f64
vikepk <prempk0516@gmail.com>
2023-08-09 17:18:51 +0530

    UI Updated

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flash/screens/welcome_screen.dart';
-import 'package:flash/screens/login_screen.dart';
 import 'package:flash/screens/registration_screen.dart';
 import 'package:flash/screens/chat_screen.dart';
 import 'firebase_options.dart';
@@ -24,8 +22,6 @@ class FlashChat extends StatelessWidget {
         // home: WelcomeScreen(),
         initialRoute: Firstpage.id,
         routes: {
-          WelcomeScreen.id: (context) => WelcomeScreen(),
-          LoginScreen.id: (context) => LoginScreen(),
           RegistrationScreen.id: (context) => RegistrationScreen(),
           ChatScreen.id: (context) => ChatScreen(),
           Firstpage.id: (context) => Firstpage(),
